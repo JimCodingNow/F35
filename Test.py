5733d2be9079a06cacfdfb6c0adb87a9edde2aa5
JimCodingNow <123177140+JimCodingNow@users.noreply.github.com>
2023-02-21 08:06:24 -0500

    latest

--- Test.py
+++ Test.py
@@ -3,10 +3,12 @@ import argparse
 def main():
     parser = argparse.ArgumentParser(description="Build Test")
     parser.add_argument('--source')
+    parser.add_argument('--update')
     
     args, unknown = parser.parse_known_args()
 
     print("Args parse ", args.source)
+    print("Args parse update", args.update)
 
 if __name__ == '__main__':
     print("Inside Test1")
